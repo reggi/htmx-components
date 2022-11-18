dcc67b602e406d5bd46006c245c03d04750b4879
Thomas Reggi <thomas@reggi.com>
2022-11-17 21:28:57 -0500

    trying no import in lib

--- import_library.ts
+++ import_library.ts
@@ -1,13 +1,11 @@
-import { metaDir } from "meta";
-export const basePath = metaDir()
 
 export const library = {
   "./client_code/multi.ts": {
-    path: `${basePath}/examples/client_code/multi.ts`,
+    path: `./examples/client_code/multi.ts`,
     code: () => import("./examples/client_code/multi.ts")
   },
   "./client_code/edit_one.ts": {
-    path: `${basePath}/examples/client_code/edit_one.ts`,
+    path: `./examples/client_code/edit_one.ts`,
     code: () => import("./examples/client_code/edit_one.ts")
   },
 }
